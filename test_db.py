982467c23dd7b463379425602fe49ef0f4a7d573
David Kempkens <david.kempkens@gmail.com>
2024-08-05 10:45:14 +0200

    feat: Add unit tests for slots and db module

---
+++ test_db.py
@@ -0,0 +1,146 @@
+import os
+import sqlite3
+import unittest
+import browser.database as db
+import browser.slots as slots
+from browser.slots import Slot as Slot
+
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        db_file = "file:mem1?mode=memory&cache=shared"
+        # create database
+        self.db = sqlite3.connect(db_file, uri=True)
+        # db.reset_db(db=self.db, leave_connection_open=True)
+        db.init_db(db=self.db, leave_connection_open=True)
+        # set tracebacks
+        # self.db.set_trace_callback(print)
+
+    def test_first_time_slot_not_in_updated(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(a, db=self.db, city="Duesseldorf")
+
+        # check if the slot is not in the updated slots
+        self.assertNotIn(a, updated)
+        print(
+            "New slots should not be in the updated slots, the first time they are inserted"
+        )
+
+    def test_new_slot_in_new_slots(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(a, db=self.db, city="Duesseldorf")
+
+        # check if the slot is not in the new slots
+        self.assertIn(a, new)
+        print("New slots should be in the new slots, the first time they are inserted")
+
+    def test_open_slot_found_the_second_time(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(
+            a, db=self.db, leave_connection_open=True, city="Duesseldorf"
+        )
+
+        # insert the same slot again
+        open, updated, new = db.update_slots(a, db=self.db, city="Duesseldorf")
+
+        # check if the slot is in the updated slots
+        self.assertNotIn(a, updated)
+        print(
+            "Open slots should not be in the updated slots, the second time they found"
+        )
+
+    def test_open_slot_not_in_new_slots(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(
+            a, db=self.db, leave_connection_open=True, city="Duesseldorf"
+        )
+
+        # insert the same slot again
+        open, updated, new = db.update_slots(a, db=self.db, city="Duesseldorf")
+
+        # check if the slot is not in the new slots
+        self.assertNotIn(a, new)
+        print("Open slots should not be in the new slots, the second time they found")
+
+    def test_open_slot_in_open_slots_the_second_time(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(
+            a, db=self.db, leave_connection_open=True, city="Duesseldorf"
+        )
+
+        # insert the same slot again
+        open, updated, new = db.update_slots(a, db=self.db, city="Duesseldorf")
+
+        # check if the slot is in the open slots
+        self.assertIn(a, open)
+        print("Open slots should be in the open slots, the second time they are found")
+
+    def test_open_slot_in_db_not_found_this_time(self):
+
+        # create Test Slots
+        a = Slot("Bilk", "2021-08-01 08:00:00")
+
+        b = Slot("Bilk", "2021-08-01 09:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(
+            a, db=self.db, leave_connection_open=True, city="Duesseldorf"
+        )
+
+        # insert a different slot
+        open, updated, new = db.update_slots(b, db=self.db, city="Duesseldorf")
+
+        # check if the slot is not in the db
+        self.assertNotIn(a, open)
+
+    def test_different_city_doesnt_get_updated(self):
+
+        # create Test Slots
+        open_slot_in_duesseldorf = Slot("Bilk", "2021-08-01 08:00:00")
+
+        open_slot_in_bremen = Slot("BuergerServiceCenter-Mitte", "2021-08-01 08:00:00")
+
+        # insert new slot via update_slots
+        open, updated, new = db.update_slots(
+            open_slot_in_duesseldorf,
+            db=self.db,
+            leave_connection_open=True,
+            city="Duesseldorf",
+        )
+
+        # insert a different slot
+        open, updated, new = db.update_slots(
+            open_slot_in_bremen,
+            db=self.db,
+            city="Bremen",
+        )
+
+        # check if the slot in duesseldorf is not in the updated slots
+
+        self.assertNotIn(open_slot_in_duesseldorf, updated)
+        print("Open slots in different cities should not be updated")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_slots.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+import unittest
+import browser.slots as slots
+from browser.slots import Slot as Slot
+
+
+class TestSlots(unittest.TestCase):
+
+    def test_difference_empty_when_same(self):
+
+        A = [("Bürgerbüro 1", "2021-08-01 08:00:00")]
+        B = [("Bürgerbüro 1", "2021-08-01 08:00:00")]
+
+        self.assertEqual(slots.difference(A, B), [])
+        print("When A and B are the same, the difference should be empty")
+
+    def test_difference_empty_when_empty(self):
+
+        A = []
+        B = []
+
+        self.assertEqual(slots.difference(A, B), [])
+        print("When A and B are empty, the difference should be empty")
+
+    def test_difference_not_empty_when_different(self):
+
+        A = [("Bürgerbüro 1", "2021-08-01 08:00:00")]
+        B = [("Bürgerbüro 2", "2021-08-01 08:00:00")]
+
+        self.assertEqual(slots.difference(A, B), A)
+        print("When A and B are different, the difference should be A")
+
+    def test_date_format_equality(self):
+
+        a = Slot("Bürgerbüro 1", "2021-08-01 08:00")
+        b = Slot("Bürgerbüro 1", "2021-08-01 08:00:00")
+
+        self.assertEqual(a, b)
+        print("Slots with different date formats should be equal")
+
+    def test_datetime_object_instanciation(self):
+
+        a = Slot("Bürgerbüro 1", datetime(2021, 8, 1, 8, 0))
+        b = Slot("Bürgerbüro 1", "2021-08-01 08:00:00")
+
+        self.assertEqual(a, b)
+        print("Datetime objects should be equal to date strings")
+
+    def test_not_different_when_updated(self):
+
+        a = Slot("Bürgerbüro 1", "2021-08-01 08:00:00")
+        b = Slot("Bürgerbüro 1", "2021-08-01 08:00:00", updated="2021-08-01 08:01:00")
+
+        A = [a]
+        B = [b]
+
+        print(
+            "When A and B are the same, but B has an updated date, the difference should be empty"
+        )
+        self.assertEqual(slots.difference(A, B), [])
+
+    def test_create_slots_from_2_tuple(self):
+
+        A = [("Bürgerbüro 1", "2021-08-01 08:00:00")]
+        B = [Slot("Bürgerbüro 1", "2021-08-01 08:00:00")]
+
+        self.assertEqual(slots.create_slots(A), B)
+        print("Slots should be created from a list of tuples")
+
+    def test_create_slots_from_3_tuple(self):
+
+        A = [("Bürgerbüro 1", "2021-08-01 08:00:00", "2021-08-01 08:01:00")]
+        B = [Slot("Bürgerbüro 1", "2021-08-01 08:00:00", "2021-08-01 08:01:00")]
+
+        self.assertEqual(slots.create_slots(A), B)
+        print("Slots should be created from a list of tuples")
+
+    def test_create_slots_from_4_tuple(self):
+
+        A = [
+            (
+                "Bürgerbüro 1",
+                "2021-08-01 08:00:00",
+                "2021-08-01 08:01:00",
+                "2021-08-01 08:02:00",
+            )
+        ]
+        B = [
+            Slot(
+                "Bürgerbüro 1",
+                "2021-08-01 08:00:00",
+                "2021-08-01 08:01:00",
+                "2021-08-01 08:02:00",
+            )
+        ]
+
+        self.assertEqual(slots.create_slots(A), B)
+        print("Slots should be created from a list of tuples")
+
+
+if __name__ == "__main__":
+    unittest.main()
