b0e1220a88d6bb6cf249176ef5e0f749282901f7
David Kempkens <david.kempkens@gmail.com>
2024-08-12 15:24:35 +0200

    feat: Add backup timestamp to backup script

--- backup.sh
+++ backup.sh
@@ -5,5 +5,7 @@ if [ ! -d backup ]; then
     mkdir backup
 fi
 
+echo "Backup at $(date +%Y-%m-%d_%H-%M-%S)"
+
 # copy the database to the backup folder
 cp database.db backup/database_$(date +%Y-%m-%d_%H-%M-%S).db
