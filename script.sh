483ce9c051905b19fdf7efe7450427ec33bdfbb2
David Kempkens <david.kempkens@gmail.com>
2024-09-02 09:05:19 +0200

    chore: parameterize bash script

--- script.sh
+++ script.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# source scraping-env/Scripts/activate
+source venv/Scripts/activate
 
-# run duesseldorf.py every minute
+# first argumtent is the city name
+city=$1
+
+# check if the city name is provided
+if [ -z "$city" ]; then
+    echo "Please provide the city name as the first argument"
+    exit 1
+fi
+
+echo "City: $city"
+
 while true; do
-    python run.py
+    python run.py $city
     sleep 60
 done
