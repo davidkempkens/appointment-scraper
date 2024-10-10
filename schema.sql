dacf73ec0e526c8cdfce1e1f5799f46c708504d8
David Kempkens <david.kempkens@gmail.com>
2024-10-10 12:25:40 +0200

    feat: simplify DB concept

--- schema.sql
+++ schema.sql
@@ -1,22 +1,17 @@
 CREATE TABLE
-    IF NOT EXISTS Stadt (name VARCHAR(255) PRIMARY KEY NOT NULL);
-
-CREATE TABLE
-    IF NOT EXISTS BuergerBuero (
-        stadtteil VARCHAR(255) PRIMARY KEY NOT NULL,
-        stadt VARCHAR(255) NOT NULL,
-        FOREIGN KEY (stadt) REFERENCES Stadt (name),
-        UNIQUE (stadtteil, stadt)
+    IF NOT EXISTS Slots (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        office VARCHAR(255) NOT NULL,
+        city VARCHAR(255) NOT NULL,
+        timeslot DATE NOT NULL,
+        concern VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE
-    IF NOT EXISTS Termin (
-        buergerbuero VARCHAR(255) NOT NULL,
-        datum DATE NOT NULL,
-        angelegenheit VARCHAR(255),
-        -- save timestamp when the appointment was first recorded 
-        erstmalsErfasstAlsFrei TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        erstmalsErfasstAlsBelegt TIMESTAMP,
-        PRIMARY KEY (datum, buergerbuero),
-        FOREIGN KEY (buergerbuero) REFERENCES BuergerBuero (stadtteil)
+    IF NOT EXISTS Availabilities (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        slot_id INTEGER NOT NULL,
+        available TIMESTAMP,
+        taken TIMESTAMP,
+        FOREIGN KEY (slot_id) REFERENCES Slots(id)
     );
